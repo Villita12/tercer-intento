5d7b742ec5a9ca3426b525793344430d97d7a6cb
Villita12 <sfpostal@gmail.com>
2022-09-27 14:37:35 -0300

    subiendo archivos

---
+++ scripts.js
@@ -0,0 +1,17 @@
+//var nombre = prompt("Cual es tu nombre?");
+//document.getElementById('mensaje').innerHTML = `Bienvenido ${nombre}`;
+ //let aprendiendo = 'Aprendiendo',
+ //tecnologia = 'Javascript';
+
+ //console.log(aprendiendo + ' ' + tecnologia);
+ const suma = (a = 0, b = 0) => a + b; 
+ 
+ const multiplicar = (a) => a * 5;
+ 
+
+ let total;
+  
+ let resultadoSuma = suma(1 + 27);
+ total = multiplicar(resultadoSuma);
+ 
+ console.log(total);
